2215f3c62ec8a12a2fb535aeccf7fc08c31bddd2
RipYashok <114049490+RipYashok@users.noreply.github.com>
2023-03-27 22:25:41 +0300

    Created a base for working with collection

---
+++ src/CollectionStructure/Color.java
@@ -0,0 +1,24 @@
+package CollectionStructure;
+
+public enum Color {
+    GREEN{
+        @Override
+        public String toString() { return "Green";}
+    },
+    BLUE{
+        @Override
+        public String toString() { return "Blue";}
+    },
+    WHITE{
+        @Override
+        public String toString() { return "White";}
+    },
+    BLACK{
+        @Override
+        public String toString() { return "Black";}
+    },
+    BROWN{
+        @Override
+        public String toString() { return "Brown";}
+    };
+}
---
+++ src/CollectionStructure/Semester.java
@@ -0,0 +1,20 @@
+package CollectionStructure;
+
+public enum Semester {
+    FIRST{
+        @Override
+        public String toString() { return "First";}
+    },
+    THIRD{
+        @Override
+        public String toString() { return "Third";}
+    },
+    SIXTH{
+        @Override
+        public String toString() { return "Sixth";}
+    },
+    SEVENTH{
+        @Override
+        public String toString() { return "Seventh";}
+    };
+}
